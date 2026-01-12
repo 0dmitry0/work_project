a324cb9274244ea23adabc419bf323d78be58806
Dmitry <123327687+0dmitry0@users.noreply.github.com>
2026-01-12 18:00:39 +0300

    Update __init__.py

--- ratnet/__init__.py
+++ ratnet/__init__.py
@@ -1,12 +1,12 @@
 from netbox.plugins import PluginConfig
 
-class RatNetConfig(PluginConfig):
-    name = 'ratnet'
-    verbose_name = 'RatNet'
-    description = 'Your RatNet plugin description'
+class GreenNetConfig(PluginConfig):
+    name = 'GreenNet'
+    verbose_name = 'GreenNet'
+    description = 'GreenNet makes everything green like grass!'
     version = '1.0'
-    author = 'Your Name'
-    author_email = 'your.email@example.com'
-    base_url = 'ratnet'
+    author = 'Aponenko Dmitry'
+    author_email = 'daponenk@gmail.com'
+    base_url = 'greennet'
 
-config = RatNetConfig
+config = GreenNetConfig
